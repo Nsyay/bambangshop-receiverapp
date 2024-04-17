a979a45b4028809084f8f1fb3c149a0bc5e04bb5
Nasya Ayu <ayu.siti21@ui.ac.id>
2024-04-17 13:48:04 +0700

    remove unnecessary parentheses around `if` condition

--- src/model/notification.rs
+++ src/model/notification.rs
@@ -18,7 +18,7 @@ impl Display for Notification{
             return write!(f,
                 "Hello {}, let's try our new {} product: {}, only in BambangShop! Check it out: {}",
                 self.subscriber_name, self.product_type.to_lowercase(), self.product_title, self.product_url )
-        }else if (self.status.to_uppercase().eq("DELETED")){
+        }else if self.status.to_uppercase().eq("DELETED") {
             return write!(f,
                 "Hello {}, we informed that out {} product called {} already sold out...",
                 self.subscriber_name, self.product_type.to_lowercase(), self.product_title);
